8d4670ff1bd28be6ba902beaaf0c047617e29806
Emre AYBAR <aybaremre5@gmail.com>
2024-01-03 15:03:08 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,42 +1,32 @@
 
-
 public class Main {
 
-    private static   long calculatefaktöriyel( long number ){
+    private static long calculateFaktöriyel(long number) {
 
-   if (number < 0){
-       System.out.println("Enter a positive number. ");
-       System.exit(1);
-   }
 
-    if (number==0||number==1){
-        System.out.println(1);
-        return 1 ;
 
-    }else {
-        long result =1;
-        for (int i = 2;i<=number;i++){
-                   result*=i;
+
+            if (number == 0 || number == 1) {
+                System.out.println(1);
+                return 1;
+            } else if (number < 0) {
+                System.out.println("Pozitif rakam giriniz ");
+
+            } else {
+                long result = 1;
+                for (int i = 2; i <= number; i++) {
+                    result *= i;
+                }
+                System.out.println("Sonucumuz :" + result);
+                return result;
+            }
+    return number;
+    }
+
+
+        public static void main (String[]args){
+
+           calculateFaktöriyel(-1);
+
         }
-        System.out.println("sonucumuz :"+result);
-        return result;
     }
-
-
-
-
-    }
-
-
-
-
-
-
-
-
-    public static void main(String[] args) {
-
-calculatefaktöriyel(100);
-
-    }
-}
